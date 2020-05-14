e9442bc56775e6cb4dee70c4b89e68a73c338b9c
rcbensley <richardbensley@gmail.com>
2020-05-14 23:58:41 +0100

    Adding batches.

--- main.go
+++ main.go
@@ -7,10 +7,13 @@ import (
 	"math/rand"
 	"os"
 	"strconv"
+	"strings"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
+
+var sessionSQL string = "SET SESSION sql_log_bin=0"
 
 type table struct {
 	name string
@@ -21,6 +24,7 @@ type brim struct {
 	db            *sql.DB
 	rowCountTotal int
 	rowsPerTable  int
+	batchSize     int
 	tableCount    int
 	databaseName  string
 	tableBaseName string
@@ -39,18 +43,18 @@ func randString(length int) string {
 }
 
 func genRow() string {
-	// 1000000 x genRow ~= 1TB
+	// 1000000000 x genRow ~= 1TB
 	rand.Seed(time.Now().UnixNano())
 	b := rand.Intn(2147483647)
 	c := randString(255)
 	d := randString(255)
 	e := randString(255)
 	f := randString(255)
-	return fmt.Sprintf("%d,'%s','%s','%s','%s'", b, c, d, e, f)
+	return fmt.Sprintf("(%d,'%s','%s','%s','%s')", b, c, d, e, f)
 }
 
 func (b *brim) createDatabase() error {
-	log.Printf("Attempting to create database %s\n", b.databaseName)
+	log.Printf("Creating database %s\n", b.databaseName)
 	create := fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s", b.databaseName)
 	err := b.exec(create)
 	if err != nil {
@@ -60,16 +64,16 @@ func (b *brim) createDatabase() error {
 }
 
 func (b *brim) createTable(name string) error {
-	log.Printf("Attempting to create table %s.%s\n", b.databaseName, name)
+	log.Printf("Creating table %s.%s\n", b.databaseName, name)
 	create := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s.%s (
-		a bigint(20) NOT NULL AUTO_INCREMENT,
-		b int(11) NOT NULL,
-		c char(255) NOT NULL,
-		d char(255) NOT NULL,
-		e char(255) NOT NULL,
-		f char(255) NOT NULL,
-		PRIMARY KEY (a),
-		INDEX (b)) ENGINE=InnoDB;`, b.databaseName, name)
+	a bigint(20) NOT NULL AUTO_INCREMENT,
+	b int(11) NOT NULL,
+	c char(255) NOT NULL,
+	d char(255) NOT NULL,
+	e char(255) NOT NULL,
+	f char(255) NOT NULL,
+	PRIMARY KEY (a),
+	INDEX (b)) ENGINE=InnoDB;`, b.databaseName, name)
 	err := b.exec(create)
 	if err != nil {
 		return err
@@ -87,18 +91,9 @@ func (b *brim) createTables() error {
 	return nil
 }
 
-// func (b *brim) query(query string) error {
-// 	rows, err := b.db.Query(query)
-// 	if err != nil {
-// 		return err
-// 	}
-// 	defer rows.Close()
-// 	fmt.Printf("%v\n", rows)
-// 	return nil
-// }
-
 func (b *brim) exec(query string) error {
-	_, err := b.db.Exec(query)
+	q := fmt.Sprintf("%s; %s;", sessionSQL, query)
+	_, err := b.db.Exec(q)
 	if err != nil {
 		return err
 	}
@@ -106,19 +101,33 @@ func (b *brim) exec(query string) error {
 }
 
 func (b *brim) load(table string) {
-	for i := 1; i <= b.rowsPerTable; i++ {
-		v := genRow()
-		row := fmt.Sprintf("INSERT INTO %s.%s (b,c,d,e,f) VALUES (%s);", b.databaseName, table, v)
+
+	for i := b.batchSize; i <= b.rowsPerTable; i = i + b.batchSize {
+		batch := make([]string, b.batchSize)
+		for a := range batch {
+			r := genRow()
+			batch[a] = r
+		}
+		joinedBatch := strings.Join(batch, ",")
+		row := fmt.Sprintf("INSERT INTO %s.%s (b,c,d,e,f) VALUES %s", b.databaseName, table, joinedBatch)
 		err := b.exec(row)
 		if err != nil {
-			log.Println(err)
+			log.Fatal(err)
 		}
+
+		// if (i + batchSize) >= b.rowsPerTable {
+		// 	batchSize = b.rowsPerTable - i
+		// }
+
+		// i = i + batchSize
+		// fmt.Printf("i is %d rows are %d batch is %d\n", i, b.rowsPerTable, batchSize)
 	}
 }
 
 func main() {
 	var rowsTotal int = 1000000000
 	var threads int = 20
+	var batchSize int = 1000
 	var err error
 
 	// With an limit of 1b rows, and a max of 100 tables, the largest table can be 10m rows.
@@ -128,8 +137,11 @@ func main() {
 			log.Fatal(err)
 		}
 		if rowsTotal > 1000000000 {
-			fmt.Println("Max of 1,000,000,000 rows.")
+			log.Println("Max of 1,000,000,000 rows.")
 			rowsTotal = 1000000000
+		}
+		if rowsTotal <= 1 {
+			rowsTotal = 1
 		}
 	}
 
@@ -139,33 +151,47 @@ func main() {
 			log.Fatal(err)
 		}
 		if threads > 100 {
-			fmt.Println("Max of 100 threads")
+			log.Println("Max of 100 threads")
 			threads = 100
+		}
+		if threads <= 1 {
+			threads = 1
+		}
+	}
+
+	if len(os.Args) >= 4 {
+		batchSize, err = strconv.Atoi(os.Args[3])
+		if err != nil {
+			log.Fatal(err)
+		}
+		if batchSize > rowsTotal {
+			log.Printf("Batch size cannot be larger can the total rows, using row limit of %d.", rowsTotal)
+			batchSize = rowsTotal
+		}
+		if batchSize <= 1 {
+			batchSize = 1
 		}
 	}
 
 	b := brim{
 		rowCountTotal: rowsTotal,
-		rowsPerTable:  rowsTotal / 100,
 		databaseName:  "brim",
 		tableBaseName: "brim",
 		threads:       threads,
+		batchSize:     batchSize,
 	}
 
-	if b.rowCountTotal < b.rowsPerTable {
-		b.tableCount = 1
-	} else {
-		b.tableCount = b.rowCountTotal / b.rowsPerTable
-	}
+	b.rowsPerTable = b.rowCountTotal / b.threads
+	b.tableCount = b.threads
 
-	tableNames := make([]table, b.tableCount)
+	tableNames := make([]table, b.threads)
 	for i := 0; i <= b.tableCount-1; i++ {
 		t := table{name: fmt.Sprintf("%s%d", b.tableBaseName, i), rows: b.rowsPerTable}
 		tableNames[i] = t
 	}
 	b.tableNames = tableNames
 
-	db, err := sql.Open("mysql", "root@unix(/var/lib/mysql/mysql.sock)/")
+	db, err := sql.Open("mysql", "root@unix(/var/lib/mysql/mysql.sock)/?multiStatements=true&autocommit=true")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -186,7 +212,8 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("Starting load of %d rows over %d table(s) with %d rows each into database %s\n", b.rowCountTotal, len(b.tableNames), b.rowsPerTable, b.databaseName)
+	log.Printf("Starting load of %d rows over %d table(s) with %d rows each,\n", b.rowCountTotal, len(b.tableNames), b.rowsPerTable)
+	log.Printf("Batch size is %d, with %d max rows per-table.\n", b.batchSize, b.rowsPerTable)
 
 	jobCount := len(b.tableNames)
 	jobs := make(chan string, jobCount)
@@ -208,7 +235,7 @@ func main() {
 
 func (b *brim) loadTable(id int, jobs <-chan string, results chan<- string) {
 	for t := range jobs {
-		fmt.Printf("Worker %d loading %s with %d rows\n", id, t, b.rowsPerTable)
+		log.Printf("Worker %d loading %s with %d rows\n", id, t, b.rowsPerTable)
 		b.load(t)
 		results <- t
 	}
